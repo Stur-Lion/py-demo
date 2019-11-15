03074327d3e6582734d98eb0644754d8101adf4a
Stur-Lion <1504468447@qq.com>
2019-11-15 13:40:15 +0800

    基础

--- 1-demo.py
+++ 1-demo.py
@@ -1,1 +1,36 @@
-print ("Hello World!")
+print("Hello World!")
+
+if False:
+    print("Haha!")
+else:
+    print("Wowo!")
+    print("Lala!")
+doc = 'ksajdljk' + \
+      'eqwe'
+print(doc)
+
+"""
+kdfjkdjfkljjljfjklfjk 
+"""
+one = "jdfkjkf";
+two = "fklsdjfkljkl";
+print(one, two)
+del one
+one = "one"
+print(one)
+
+string = "abcdefg"
+print(string)
+print(string[1:3])
+print(string[::2])
+
+obj = {
+    'one': 'one',
+    'two': 'two',
+    'three': 'three'
+}
+print(obj['one'])
+print(obj.keys())
+print(obj.values())
+
+
